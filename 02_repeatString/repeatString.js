caf0c4cbd78d47e6b4aa8b46f449742a7601c5bb
Kebur Lulseged <kb.gedion@gmail.com>
2024-04-04 09:56:15 -0700

    ex2: add error handling for negative values

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
 const repeatString = function(word, num) {
     let string = "";
+
+    if (num < 0) {
+        return "ERROR"
+    }
+    
     for (let i = 0; i < num; i++) {
         string += word;
     }
