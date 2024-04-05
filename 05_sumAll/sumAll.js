7ff748b1fc51a77e910a6431d05cd197fae0fd1b
Kebur Lulseged <kb.gedion@gmail.com>
2024-04-05 09:38:14 -0700

    ex5: complete sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let sum = 0;
 
+    if (typeof firstNum != "number" || 
+        typeof secondNum != "number") {
+        return "ERROR";
+    } else if (firstNum < 0 || secondNum < 0) {
+        return "ERROR";
+    } else if (firstNum < secondNum) {
+        for (let i = firstNum; i <= secondNum; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = secondNum; i <= firstNum; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
