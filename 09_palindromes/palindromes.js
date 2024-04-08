3b24bf080b1ab9b8aaa57fe237ca6cb1b700b8dd
Kebur Lulseged <kb.gedion@gmail.com>
2024-04-08 10:30:32 -0700

    ex9: Complete palindromes for all conditions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let string = "";
+    let adjustedWord = word.toLowerCase().split("");
+    let trimmedWord = [];
 
+    // filter out punctuation
+    for (let i = 0; i < word.length; i++) {
+        let char = adjustedWord[i];
+        if (!(char >= 'a' && char <= 'z') && 
+            !(char >= '0' && char <= '9')) {
+            continue;
+        } else {
+            trimmedWord.unshift(char);
+        }
+    }
+    // reverse string
+    trimmedWord = trimmedWord.join("");
+    for (let i = trimmedWord.length - 1; i >= 0; i--){
+        string += trimmedWord.at(i);
+    }
+
+    // compare
+    return (string === trimmedWord)? true : false;
 };
 
 // Do not edit below this line
