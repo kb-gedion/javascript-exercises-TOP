b778213412f1fcc143f100a47909ed019ec0f954
Kebur Lulseged <kb.gedion@gmail.com>
2024-04-08 09:28:32 -0700

    ex8: Complete all functionality for the calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach(element => {
+    sum += +element
+  });
+
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let first = arr[0];
+  for (let i = 1; i < arr.length; i++)
+    first *= arr[i];
+  return first;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let factorial = 1;
+  for (let i = 1; i <= x; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
