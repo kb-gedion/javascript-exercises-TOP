b8221d870307b12803d73fc2b174239bcc4f3c46
Kebur Lulseged <kb.gedion@gmail.com>
2024-04-08 12:01:59 -0700

    ex12: complete findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = ""
+    let years = 0;
+    let current = 0;
+    const currentYear = new Date().getFullYear();
 
+    for (let i = 0; i < people.length; i++) {
+        if (!people[i].hasOwnProperty('yearOfDeath')) {
+            current = currentYear- people[i].yearOfBirth;
+        } else {
+            current = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+        if (current > years) {
+            years = current;
+            oldest = people[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
