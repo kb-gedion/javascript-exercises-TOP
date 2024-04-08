70520e83d5f4a696523e1e63718c229ecdc62fcc
Kebur Lulseged <kb.gedion@gmail.com>
2024-04-08 10:53:54 -0700

    ex10: Complete fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let fib = 1;
+    let fibNext = 1;
+    let nth = 0;
 
+    if (n < 0)
+        return 'OOPS'
+
+    switch (+n) {
+        case 0:
+            return 0;
+            break;
+        case 1: 
+        case 2:
+            return 1;
+            break;
+    }
+
+    for (let i = 3; i <= n; i++) {
+        nth = fib + fibNext;
+        fibNext = fib;
+        fib = nth;
+    }
+    return fib;
 };
 
 // Do not edit below this line
